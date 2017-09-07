a1f54e4535c7a3eb24aa8851839546494c218397
Kino <p@kinon.sakura.ne.jp>
2017-09-07 23:59:23 +0900

    Put supporting functions out

--- src/parser.rs
+++ src/parser.rs
@@ -3,6 +3,32 @@ extern crate combine;
 use self::combine::*;
 use ::data::TheData;
 
+fn parse_i32<I>(input: I) -> ParseResult<i32, I>
+    where I: Stream<Item=char> {
+
+    many1::<String, _>(char::digit())
+        .map(|s| s.parse::<i32>().unwrap())
+        .parse_stream(input)
+}
+
+fn parse_num<I>(input: I) -> ParseResult<i32, I>
+    where I: Stream<Item=char> {
+
+    char::string("num")
+        .with(char::spaces())
+        .with(parser(parse_i32))
+        .parse_stream(input)
+}
+
+fn parse_vec<I>(input: I) -> ParseResult<Vec<i32>, I>
+    where I: Stream<Item=char> {
+
+    char::string("vec")
+        .with(char::spaces())
+        .with(sep_by1(parser(parse_i32), token(',')))
+        .parse_stream(input)
+}
+
 pub struct TheParser {}
 
 impl TheParser {
@@ -10,39 +36,13 @@ impl TheParser {
         TheParser {}
     }
 
-    fn parse_i32<I>(&self, input: I) -> ParseResult<i32, I>
-        where I: Stream<Item=char> {
-
-        many1::<String, _>(char::digit())
-            .map(|s| s.parse::<i32>().unwrap())
-            .parse_stream(input)
-    }
-
-    fn parse_num<I>(&self, input: I) -> ParseResult<i32, I>
-        where I: Stream<Item=char> {
-
-        char::string("num")
-            .with(char::spaces())
-            .with(parser(|x| self.parse_i32(x)))
-            .parse_stream(input)
-    }
-
-    fn parse_vec<I>(&self, input: I) -> ParseResult<Vec<i32>, I>
-        where I: Stream<Item=char> {
-
-        char::string("vec")
-            .with(char::spaces())
-            .with(sep_by1(parser(|x| self.parse_i32(x)), token(',')))
-            .parse_stream(input)
-    }
-
     pub fn parse<I>(&self, input: I) -> ParseResult<TheData, I>
         where I: Stream<Item=char> {
 
         let mut p =
-            sep_by(parser(|x| self.parse_num(x)), char::spaces())
+            sep_by(parser(parse_num), char::spaces())
             .skip(token(';'))
-            .and(sep_by(parser(|x| self.parse_vec(x)), char::spaces()))
+            .and(sep_by(parser(parse_vec), char::spaces()))
             .map(|t| {
                 let (ns, vs) = t;
                 TheData { num: ns, vec: vs }
